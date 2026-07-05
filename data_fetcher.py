a49ad3f2213f35ce719c5f31c1abfe151a78ba38
Aylin Bahar <aylin_65@proton.me>
2026-07-05 19:44:01 +0200

    added new file to modulize fetching data

---
+++ data_fetcher.py
@@ -0,0 +1,39 @@
+import requests
+
+
+
+API_KEY = "F1VH1NjfjZPoJdfDcXu6loDmleH3BpcMS4vyokYC" #kommt später in .env
+
+
+def fetch_data(animal_name):
+  """
+  Fetches the animals data for the animal 'animal_name'.
+  Returns: a list of animals, each animal is a dictionary:
+  {
+    'name': ...,
+    'taxonomy': {
+      ...
+    },
+    'locations': [
+      ...
+    ],
+    'characteristics': {
+      ...
+    }
+  },
+  """
+  response = requests.get(
+          "https://api.api-ninjas.com/v1/animals",
+          headers={"X-Api-Key": API_KEY},
+          params={"name": animal_name}
+  )
+  return response.json()
+
+# def load_data_from_api(animal_name):
+#     """gets animal data from the ninja animals API."""
+#     response = requests.get(
+#         "https://api.api-ninjas.com/v1/animals",
+#         headers={"X-Api-Key": API_KEY},
+#         params={"name": animal_name}
+#     )
+#     return response.json()
